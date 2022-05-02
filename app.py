b0811a781d956c4b79389d71fac173cf20edc1a1
Darren MelRoy Menezes <38250193+dmMenezes@users.noreply.github.com>
2022-05-03 02:09:09 +0400

    update

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from binance.enums import *
 
 app = Flask(__name__)
 
-#client = Client(config.API_KEY, config.API_SECRET, tld='us')
 client = Client(config.API_KEY, config.API_SECRET, tld = 'com')
 
 def order(side, quantity, symbol, order_type=ORDER_TYPE_MARKET):
